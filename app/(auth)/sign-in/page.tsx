15efe51cb7f285067d7e2857904da97f80fc4193
dev-smashik <dev.smashik@gmail.com>
2025-07-15 00:38:46 +0600

    update

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+
+import AuthForm from "@/components/AuthForm";
+import React from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  return <AuthForm type="sign-in"/>;
+  
+};
 
-export default page
+export default page;
